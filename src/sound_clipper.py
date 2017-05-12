5eac6e6993fb0e5ba0ccdb66213a1148952dbb02
Jared <jmw535@cornell.edu>
2017-05-12 11:35:11 -0400

    threading the sound clipper

--- src/sound_clipper.py
+++ src/sound_clipper.py
@@ -7,13 +7,30 @@ import sys, os, math
 import wave, struct
 import analyze
 import settings, audio
+import threading
 
 IN_DIR = '../data/sound_clipper_sources'
 OUT_DIR = '../data/samples'
-counter = 0
 
-for fname in os.listdir(IN_DIR):
-    if fname.endswith('.wav'):
+def generate_sounds():
+    for fname in os.listdir(IN_DIR):
+        if fname.endswith('.wav') or fname.endswith('.mp3'):
+            FileSplitter(fname).start()
+    if settings.FREQUENCY_SPLIT:
+        for fname in os.listdir(OUT_DIR):
+            if fname.endswith('.wav') or fname.endswith('.mp3'):
+                OutfileSplitter(fname).start()
+
+class FileSplitter(threading.Thread):
+    def __init__(self, fname):
+        self.fname = fname
+        threading.Thread.__init__ ( self )
+
+    def run(self):
+        self.split_single_file(self.fname)
+
+    def split_single_file(self, fname):
+        counter = 0
         current_file = wave.open(IN_DIR + '/' + fname, 'r')
         pulse_loc = analyze.pulse_detect(IN_DIR + '/' + fname, settings.PULSE_TYPE)
         prev_point = 0
@@ -29,10 +46,16 @@ for fname in os.listdir(IN_DIR):
             working.close()
             prev_point = pulse_point
 
-if settings.FREQUENCY_SPLIT:
-    for fname in os.listdir(OUT_DIR):
-        if fname.endswith('wav'):
-            audio.split_file(OUT_DIR + '/' + fname)
-            os.remove(OUT_DIR + '/' + fname)
+class OutfileSplitter(threading.Thread):
+    def __init__(self, fname):
+        self.fname = fname
+        threading.Thread.__init__ ( self )
+
+    def run(self):
+        self.split_outfile(self.fname)
+
+    def split_outfile(self, fname):
+        audio.split_file(OUT_DIR + '/' + fname)
+        os.remove(OUT_DIR + '/' + fname)
 
 
